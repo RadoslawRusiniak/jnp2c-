9080651b9c4177af161559dfdfd36bb310e26d5f
Radoslaw Rusiniak <radoslaw.rusiniak@interopeo.com>
2014-08-15 04:38:43 +0200

    Pierwszy commit

---
+++ WpfProject/GamePage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//DispacherTimer
+using System.Windows.Threading;
+
+namespace WpfProject
+{
+    /// <summary>
+    /// Interaction logic for GamePage.xaml
+    /// </summary>
+    public partial class GamePage : Page
+    {
+        enum MOVINGDIRECTION { LEFT, RIGHT, DOWN, UP, NONE };
+        private Point startingPoint = new Point(30, 30);
+        private Point currentPosition;
+        private MOVINGDIRECTION direction = MOVINGDIRECTION.NONE;
+        private int shipNr = 12345;
+
+        public GamePage()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+
+            /* Here user can change the speed of the snake.
+             * Possible speeds are FAST, MODERATE, SLOW and DAMNSLOW */
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Start();
+
+            Application.Current.MainWindow.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+            paintShip(startingPoint);
+            currentPosition = startingPoint;
+        }
+
+        private void paintShip(Point currentposition)
+        {
+            /* This method is used to paint a frame of the snake´s body
+             * each time it is called. */
+            if (GameCanvas.Children.Count >= shipNr)
+            {
+                GameCanvas.Children.RemoveAt(shipNr);
+            }
+
+            Ellipse newEllipse = new Ellipse();
+            newEllipse.Fill = Brushes.Aqua;
+            newEllipse.Width = 20;
+            newEllipse.Height = 20;
+
+            Canvas.SetTop(newEllipse, currentposition.Y);
+            Canvas.SetLeft(newEllipse, currentposition.X);
+
+            shipNr = GameCanvas.Children.Add(newEllipse);
+        }
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    direction = MOVINGDIRECTION.LEFT;
+                    break;
+                case Key.Right:
+                    direction = MOVINGDIRECTION.RIGHT;
+                    break;
+                case Key.Up:
+                    direction = MOVINGDIRECTION.UP;
+                    break;
+                case Key.Down:
+                    direction = MOVINGDIRECTION.DOWN;
+                    break;
+            }
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Expand the body of the snake to the direction of movement
+            switch (direction)
+            {
+                case MOVINGDIRECTION.LEFT:
+                    currentPosition.X -= 5;
+                    paintShip(currentPosition);
+                    break;
+                case MOVINGDIRECTION.RIGHT:
+                    currentPosition.X += 5;
+                    paintShip(currentPosition);
+                    break;
+                case MOVINGDIRECTION.UP:
+                    currentPosition.Y -= 5;
+                    paintShip(currentPosition);
+                    break;
+                case MOVINGDIRECTION.DOWN:
+                    currentPosition.Y += 5;
+                    paintShip(currentPosition);
+                    break;
+            }
+            direction = MOVINGDIRECTION.NONE;
+
+            // Restrict to boundaries of the Canvas
+            if ((currentPosition.X < 0) || (currentPosition.X > 400) ||
+                (currentPosition.Y < 0) || (currentPosition.Y > 500))
+                GameOver();
+        }
+
+        private void DirectionShow()
+        {
+            MessageBox.Show("Direction " + direction, "", MessageBoxButton.OK, MessageBoxImage.Hand);
+        }
+
+        private void GameOver()
+        {
+            MessageBox.Show("You Lose! Game Over", "",MessageBoxButton.OK, MessageBoxImage.Hand);
+            this.NavigationService.GoBack();
+        }
+    }
+}
---
+++ WpfProject/ProjectHome.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectHome.xaml
+    /// </summary>
+    public partial class ProjectHome : Page
+    {
+        public ProjectHome()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayClick(object sender, RoutedEventArgs e)
+        {
+            GamePage gamePage = new GamePage();
+            this.NavigationService.Navigate(gamePage);
+        }
+
+        private void HighScoresClick(object sender, RoutedEventArgs e)
+        {
+            HighScoresPage highScoresPage = new HighScoresPage();
+            this.NavigationService.Navigate(highScoresPage);
+        }
+    }
+}
